febccbba9049f78c97ae1de29723be39faaa95b8
crack <crack>
2019-03-01 22:18:44 +0900

    add appbar in search_page

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -10,6 +10,7 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
